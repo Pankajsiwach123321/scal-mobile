89fe2ddd1530c4e3755c50fb61f3d2e6f797dfa0
Pankajsiwach123321 <pankusiwach000@gmai.com>
2023-11-25 13:58:16 +0530

    add animation in ellipes

--- tailwind.config.js
+++ tailwind.config.js
@@ -26,7 +26,8 @@ export default {
       },
       animation: {
         'load': 'wiggle 1000ms var(--d) linear infinite',
-        
+        'ellipes': 'eon 6000ms  linear infinite alternate-reverse',
+
       },
       keyframes: {
         wiggle: {
@@ -38,6 +39,24 @@ export default {
           '50%': {
             opacity: 0.7,
             transform: 'translateY(0)',
+          },
+        },
+        eon: {
+          '0%': {
+            scale: '0.5'
+          },
+
+          '25%': {
+            scale: '0.7'
+          },
+          '50%': {
+            scale: '1'
+          },
+          '75%': {
+            scale: '0.7'
+          },
+          '100%': {
+            scale: '0.5'
           },
         },
       }
